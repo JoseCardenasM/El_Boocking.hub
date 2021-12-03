c69983dd607151c16f27f1a69d33a0febe6fa8b4
JoseCardenasM <belisariocardenas300@gmail.com>
2021-12-03 13:14:13 -0500

    hub

--- frontend/src/apis/TheBookingCRUD.js
+++ frontend/src/apis/TheBookingCRUD.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const url = "http://localhost:5000/";
-const booking = "bookings"
+const booking = "El_Boocking.hub"
 
 export function getCrudBookings(callback){
   axios.get(url + booking)
