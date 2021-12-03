a4c0922bfbafe2702c4ffd1c2f52b624da3dc80c
JoseCardenasM <belisariocardenas300@gmail.com>
2021-12-03 16:05:00 -0500

    /delt

--- frontend/src/apis/TheBookingCRUD.js
+++ frontend/src/apis/TheBookingCRUD.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = "https://bookifront.herokuapp.com/";
+const url = "https://bookifront.herokuapp.com";
 const booking = "El_Boocking.hub"
 
 export function getCrudBookings(callback){
